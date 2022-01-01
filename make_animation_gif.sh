5c809cdefd391d955f7b0aa7e05f1a42d03a57ba
yKesamaru <y.kesamaru@tokai-kaoninsho.com>
2022-01-02 07:41:09 +0900

    [add] zenity

--- make_animation_gif.sh
+++ make_animation_gif.sh
@@ -10,10 +10,11 @@ QUALITY=0-5
 # ToDo
 # zinity OR other GUI system
 
-ffmpeg -i *.mp4 -an -r $FPS %04d.png 
-mogrify -resize ${WIDTH}x *.png
-pngquant --posterize=$POSTERIZE --quality=$QUALITY *.png
-convert *fs8.png ANIMATION.gif
-rm *.png
-paplay "other/Positive.ogg"
-notify-send -i "other/mark_heart.png" "アニメーションGIF" "終了しました"
+file=$(zenity --file-selection);
+ffmpeg -i ${file} -an -r $FPS %04d.png;
+# mogrify -resize ${WIDTH}x *.png
+# pngquant --posterize=$POSTERIZE --quality=$QUALITY *.png
+# convert *fs8.png ANIMATION.gif
+# rm *.png
+# paplay "other/Positive.ogg"
+# notify-send -i "other/mark_heart.png" "アニメーションGIF" "終了しました"
